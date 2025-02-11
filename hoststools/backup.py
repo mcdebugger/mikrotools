5cb65d00cc87ffda8157b867a6735c6a2a701f57
Maksim Kachur <me@maksimkachur.ru>
2025-02-11 22:53:08 +0300

    Add support for sensitive flag for config backups

--- hoststools/backup.py
+++ hoststools/backup.py
@@ -1,17 +1,38 @@
+from packaging import version
+
 from tools.colors import fcolors_256 as fcolors
 from tools.ssh import HostCommandsExecutor
 
 from .models import MikrotikHost
 
-def backup_configs(addresses):
+def backup_configs(addresses, sensitive=False):
     counter = 1
     
     for address in addresses:
         host = MikrotikHost(address=address)
         executor = HostCommandsExecutor(address)
         host.identity = executor.execute_command(':put [/system identity get name]')
+        host.installed_routeros_version = executor.execute_command(':put [/system package update get installed-version]')
+        
         print_backup_progress(host, counter, len(addresses), len(addresses) - counter + 1)
-        current_config = executor.execute_command('/export')
+        
+        # Exporting current config
+        if sensitive:
+            # Exporting sensitive config
+            if version.parse(host.installed_routeros_version) >= version.parse('7.0'):
+                # RouterOS 7.0+
+                current_config = executor.execute_command('/export show-sensitive')
+            else:
+                # RouterOS < 7.0
+                current_config = executor.execute_command('/export')
+        else:
+            # Exporting non-sensitive config
+            if version.parse(host.installed_routeros_version) >= version.parse('7.0'):
+                # RouterOS 7.0+
+                current_config = executor.execute_command('/export')
+            else:
+                # RouterOS < 7.0
+                current_config = executor.execute_command('/export hide-sensitive')
         with open(f'{host.identity}.rsc', 'w') as f:
             f.write(current_config)
         del executor
