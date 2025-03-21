80f2dd97db06f1037aeca8a3b1254a6ce577ef7f
Maksim Kachur <me@maksimkachur.ru>
2025-03-22 02:45:57 +0300

    Add option groups

--- src/mikrotools/plugins/execute/cli.py
+++ src/mikrotools/plugins/execute/cli.py
@@ -1,5 +1,7 @@
 import click
 
+from click_option_group import optgroup, RequiredMutuallyExclusiveOptionGroup
+
 from mikrotools.cli.utils import common_options, Mutex
 from mikrotools.mikromanager import mikromanager_init
 from mikrotools.tools.config import get_hosts, get_commands
@@ -7,8 +9,9 @@ from mikrotools.tools.config import get_hosts, get_commands
 from .utils import execute_hosts_commands
 
 @click.command(name='exec', help='Execute commands on hosts')
-@click.option('-e', '--execute-command', cls=Mutex, not_required_if=['commands_file'])
-@click.option('-C', '--commands-file', cls=Mutex, not_required_if=['execute_command'])
+@optgroup.group('Commands to execute', cls=RequiredMutuallyExclusiveOptionGroup)
+@optgroup.option('-e', '--execute-command')
+@optgroup.option('-C', '--commands-file')
 @mikromanager_init
 @common_options
 def execute(*args, **kwargs):
