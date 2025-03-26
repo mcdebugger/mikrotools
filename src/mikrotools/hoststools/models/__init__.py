cf9c5dbf341ee7a2aa778f273ddec4f62d454a1f
Maksim Kachur <me@maksimkachur.ru>
2025-03-27 02:21:21 +0300

    Implement universal Progress class using operation types

--- src/mikrotools/hoststools/models/__init__.py
+++ src/mikrotools/hoststools/models/__init__.py
@@ -1,7 +1,10 @@
 from .base import Host
 from .mikrotik import MikrotikHost
+from .operations import OperationType, OperationTemplate
 
 __all__ = [
     'Host',
-    'MikrotikHost'
+    'MikrotikHost',
+    'OperationType',
+    'OperationTemplate'
 ]
