286d0686324c070ce970d3e9c2208a19d0364bd4
Maksim Kachur <me@maksimkachur.ru>
2025-03-28 22:41:50 +0300

    Use new @coro decorator and async/await syntax instead of running asyncio.run() in the function itself

--- src/mikrotools/plugins/execute/cli.py
+++ src/mikrotools/plugins/execute/cli.py
@@ -1,11 +1,10 @@
-import asyncio
 import click
 
 from click_option_group import optgroup, RequiredMutuallyExclusiveOptionGroup
 
 from mikrotools.cli.options import common_options
 from mikrotools.mikromanager import mikromanager_init
-from mikrotools.tools.config import get_hosts, get_commands
+from mikrotools.tools import coro, get_hosts, get_commands
 
 from .utils import execute_hosts_commands
 
@@ -15,14 +14,15 @@ from .utils import execute_hosts_commands
 @optgroup.option('-C', '--commands-file', type=click.Path(exists=True))
 @mikromanager_init
 @common_options
-def execute(*args, **kwargs):
+@coro
+async def execute(*args, **kwargs):
     addresses = get_hosts()
     
     # Getting command from arguments or config file
     commands = get_commands()
     
     # Executing commands for each host in list
-    asyncio.run(execute_hosts_commands(addresses, commands))
+    await execute_hosts_commands(addresses, commands)
 
 def register(cli_group):
     cli_group.add_command(execute)
