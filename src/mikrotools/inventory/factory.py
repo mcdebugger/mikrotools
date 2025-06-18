2c86f8926dfe2bae8d7db0f3848f6cdf2f6bbece
Maksim Kachur <me@maksimkachur.ru>
2025-06-18 15:27:42 +0300

    Add netbox provider support to inventory source factory

--- src/mikrotools/inventory/factory.py
+++ src/mikrotools/inventory/factory.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 from mikrotools.config.models import InventorySourceConfig
-from mikrotools.inventory.providers import FileInventorySource, SingleInventorySource, StaticInventorySource
+from mikrotools.inventory.providers import FileInventorySource, NetboxInventorySource, SingleInventorySource, StaticInventorySource
 from mikrotools.inventory.protocol import InventorySource
 
 logger = logging.getLogger(__name__)
@@ -12,6 +12,13 @@ def get_inventory_source(config: InventorySourceConfig | None = None, source: st
         if config.type == 'file' and config.path is not None:
             logger.debug(f'get_inventory_source: Inventory source is a file: {config.file}')
             return FileInventorySource(config.path)
+        elif config.type == 'netbox':
+            if config.token is None:
+                raise ValueError('Netbox token is not specified')
+            if config.url is None:
+                raise ValueError('Netbox URL is not specified')
+            logger.debug(f'get_inventory_source: Inventory source is a Netbox: {config.url}')
+            return NetboxInventorySource(config.url, config.token, config.filters)
         elif config.type == 'static' and config.hosts is not None:
             logger.debug(f'get_inventory_source: Inventory source is a static list of hosts: {config.hosts}')
             return StaticInventorySource(config.hosts)
